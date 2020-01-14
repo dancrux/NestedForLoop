ca8d29930836f745152eac0777625f2b60f76f57
dancrux <nwogwugwudaniel@gmail.com>
2020-01-14 10:54:15 +0100

    Initial commit

---
+++ src/CruxTest.java
@@ -0,0 +1,13 @@
+//Nested for loop application
+public class CruxTest {
+    public static void main (String[] args){
+//        System.out.println("Hello World");
+        for (int s = 1; s <=12;s++){
+            for (int i =1; i <=10; i++){
+                int x= s*i;
+
+                System.out.println(s+"*"+ i + "="+(x));
+            }System.out.println("");
+        }
+     }
+}
